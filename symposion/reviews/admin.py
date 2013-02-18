26a76f4707f6eb9411aca52586ceaa794bd77a67
Luke Hatcher <lukeman@gmail.com>
2013-02-18 18:56:25 -0500

    remove symposion module specifics

--- symposion/reviews/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from symposion.reviews.models import NotificationTemplate
-
-
-admin.site.register(NotificationTemplate)
--- symposion/speakers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from symposion.speakers.models import Speaker
-
-
-admin.site.register(Speaker,
-    list_display = ["name", "email", "created"],
-    search_fields = ["name"],
-)
