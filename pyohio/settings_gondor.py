22fb98b390b8ff0215653a0a6f53b52be217415b
Luke Hatcher <lukeman@gmail.com>
2013-04-09 18:23:11 -0400

    email settings

--- pyohio/settings_gondor.py
+++ pyohio/settings_gondor.py
@@ -58,3 +58,13 @@ LOGGING = {
         },
     }
 }
+
+DEFAULT_FROM_EMAIL = "PyOhio 2013 <no-reply@pyohio.org>"
+
+if "GONDOR_SENDGRID_USER" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.sendgrid.net"
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = os.environ["GONDOR_SENDGRID_USER"]
+    EMAIL_HOST_PASSWORD = os.environ["GONDOR_SENDGRID_PASSWORD"]
+    EMAIL_USE_TLS = True
