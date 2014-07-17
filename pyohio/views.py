d07d87f1facd0b37e57899cf112e12fcfbf1028f
Sheila Miguez <github@codersquid.com>
2014-07-17 18:13:51 -0500

    include all slots in schedule json

--- pyohio/views.py
+++ pyohio/views.py
@@ -25,32 +25,30 @@ def schedule_json(request):
     slots = Slot.objects.all().order_by("start")
     data = []
     for slot in slots:
-        if slot.kind.label in ["talk", "tutorial", "plenary"] and slot.content and slot.content.proposal.kind.slug in ["talk", "tutorial"]:
-            if hasattr(slot.content.proposal, "recording_release"):
-                slot_data = {
-                    "name": slot.content.title,
-                    "room": ", ".join(room["name"] for room in slot.rooms.values()),
-                    "start": datetime.combine(slot.day.date, slot.start).isoformat(),
-                    "end": datetime.combine(slot.day.date, slot.end).isoformat(),
-                    "duration": duration(slot.start, slot.end),
-                    "authors": [s.name for s in slot.content.speakers()],
-                    "released": slot.content.proposal.recording_release,
-                    "license": "",
-                    "contact": [s.email for s in slot.content.speakers()] if request.user.is_staff else ["redacted"],
-                    "abstract": slot.content.abstract.raw,
-                    "description": slot.content.description.raw,
-                    "conf_key": slot.pk,
-                    "conf_url": "https://%s%s" % (
-                        Site.objects.get_current().domain,
-                        reverse("schedule_presentation_detail", args=[slot.content.pk])
-                    ),
-
-                    "kind": slot.content.proposal.kind.slug,
-                    "tags": "",
-                }
-        elif slot.kind.label == "lightning":
+        if hasattr(slot.content, "proposal"):
             slot_data = {
-                "name": slot.content_override.raw if slot.content_override else "Lightning Talks",
+                "name": slot.content.title,
+                "room": ", ".join(room["name"] for room in slot.rooms.values()),
+                "start": datetime.combine(slot.day.date, slot.start).isoformat(),
+                "end": datetime.combine(slot.day.date, slot.end).isoformat(),
+                "duration": duration(slot.start, slot.end),
+                "authors": [s.name for s in slot.content.speakers()],
+                "released": hasattr(slot.content.proposal, "recording_release") and slot.content.proposal.recording_release,
+                "license": "",
+                "contact": [s.email for s in slot.content.speakers()] if request.user.is_staff else ["redacted"],
+                "abstract": slot.content.abstract.raw,
+                "description": slot.content.description.raw,
+                "conf_key": slot.pk,
+                "conf_url": "https://%s%s" % (
+                    Site.objects.get_current().domain,
+                    reverse("schedule_presentation_detail", args=[slot.content.pk])
+                ),
+                "kind": slot.content.proposal.kind.slug,
+                "tags": "",
+            }
+        else:
+            slot_data = {
+                "name": slot.content_override.raw if slot.content_override else "General Break",
                 "room": ", ".join(room["name"] for room in slot.rooms.values()),
                 "start": datetime.combine(slot.day.date, slot.start).isoformat(),
                 "end": datetime.combine(slot.day.date, slot.end).isoformat(),
@@ -59,15 +57,13 @@ def schedule_json(request):
                 "released": True,
                 "license": "",
                 "contact": None,
-                "abstract": "Lightning Talks",
-                "description": "Lightning Talks",
+                "abstract": "",
+                "description": "",
                 "conf_key": slot.pk,
                 "conf_url": None,
                 "kind": slot.kind.label,
                 "tags": "",
             }
-        else:
-            continue
         data.append(slot_data)
     
     return HttpResponse(
