f17aa5ca7c95b6e46b76ff6f04a240caf820eef5
Sheila Miguez <github@codersquid.com>
2014-07-24 10:03:02 -0500

    add protocol for conference url, refactored things, dropped fields that had no data, added CC-BY for license, json response should be in dict since naked arrays are discouraged due to security exploit

--- pyohio/views.py
+++ pyohio/views.py
@@ -23,50 +23,37 @@ def duration(start, end):
 
 def schedule_json(request):
     slots = Slot.objects.all().order_by("start")
+    protocol = request.META.get('HTTP_X_FORWARDED_PROTO', 'http')
     data = []
     for slot in slots:
+        slot_data = {
+            "name": slot.content_override.raw if slot.content_override else "Slot",
+            "room": ", ".join(room["name"] for room in slot.rooms.values()),
+            "rooms": [room["name"] for room in slot.rooms.values()],
+            "start": datetime.combine(slot.day.date, slot.start).isoformat(),
+            "end": datetime.combine(slot.day.date, slot.end).isoformat(),
+            "duration": duration(slot.start, slot.end),
+            "kind": slot.kind.label,
+            "conf_key": slot.pk,
+            "license": "CC BY",  # this should be configurable or a part of the model
+        }
         if hasattr(slot.content, "proposal"):
-            slot_data = {
+            slot_data.update({
                 "name": slot.content.title,
-                "room": ", ".join(room["name"] for room in slot.rooms.values()),
-                "start": datetime.combine(slot.day.date, slot.start).isoformat(),
-                "end": datetime.combine(slot.day.date, slot.end).isoformat(),
-                "duration": duration(slot.start, slot.end),
                 "authors": [s.name for s in slot.content.speakers()],
                 "released": hasattr(slot.content.proposal, "recording_release") and slot.content.proposal.recording_release,
-                "license": "",
                 "contact": [s.email for s in slot.content.speakers()] if request.user.is_staff else ["redacted"],
                 "abstract": slot.content.abstract.raw,
                 "description": slot.content.description.raw,
-                "conf_key": slot.pk,
-                "conf_url": "https://%s%s" % (
+                "conf_url": "%s://%s%s" % (
+                    protocol,
                     Site.objects.get_current().domain,
                     reverse("schedule_presentation_detail", args=[slot.content.pk])
                 ),
-                "kind": slot.content.proposal.kind.slug,
-                "tags": "",
-            }
-        else:
-            slot_data = {
-                "name": slot.content_override.raw if slot.content_override else "General Break",
-                "room": ", ".join(room["name"] for room in slot.rooms.values()),
-                "start": datetime.combine(slot.day.date, slot.start).isoformat(),
-                "end": datetime.combine(slot.day.date, slot.end).isoformat(),
-                "duration": duration(slot.start, slot.end),
-                "authors": None,
-                "released": True,
-                "license": "",
-                "contact": None,
-                "abstract": "",
-                "description": "",
-                "conf_key": slot.pk,
-                "conf_url": None,
-                "kind": slot.kind.label,
-                "tags": "",
-            }
+            })
         data.append(slot_data)
-    
+
     return HttpResponse(
-        json.dumps(data, default=json_serializer),
+        json.dumps({'schedule': data}, default=json_serializer),
         content_type="application/json"
     )
