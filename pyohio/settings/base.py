9c8d2148220be5010c0c764b1ec5cf7346b6e1bc
Raymond Chandler III <raymondchandleriii@gmail.com>
2015-02-28 15:51:35 -0500

    Updated styles.

--- pyohio/settings/base.py
+++ pyohio/settings/base.py
@@ -34,7 +34,7 @@ MANAGERS = ADMINS
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "pyohio2014",                       # Or path to database file if using sqlite3.
+        "NAME": "pyohio2015",                       # Or path to database file if using sqlite3.
         "USER": "",                             # Not used with sqlite3.
         "PASSWORD": "",                         # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
