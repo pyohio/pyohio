9a1893952868cd9366b810899b0e53c75b8e3a0e
W. Matthew Wilson <matt@216software.com>
2015-06-16 11:44:57 -0400

    moved a file

--- pyohio/junkdrawer/grab-db-and-write-CSV.sh
+++ pyohio/junkdrawer/grab-db-and-write-CSV.sh
@@ -12,7 +12,7 @@ cd /home/matt/checkouts/pyohio2015
 
 /usr/bin/psql --quiet pyohio2015 < /tmp/pyohio2015.pg_dump > /dev/null
 
-cd mattcrap
+cd pyohio/junkdrawer
 
 /home/matt/.virtualenvs/pyohio2015/bin/python talks_with_times_and_votes.py
 
